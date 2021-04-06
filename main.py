f86072191b737044fa0314cd3663eba017ca9abc
lzhang39 <>
2021-04-06 16:29:24 +0000

    importing fixes?

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #red_blob = Blob(RED)
+#Blob().__init__(self, color, x_boundary, y_boundary)
 
 import pygame
 import random
-import Blob
+
+from blob import Blob
 
 
 STARTING_BLUE_BLOBS = 10
@@ -15,36 +17,30 @@ BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 
 game_display = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Blob World")
+pygame.display.set_caption(“Blob World”)
 clock = pygame.time.Clock()
 
-class BlueBlob(Blob):
-  def __init__(self, color, x_boundary, y_boundary):
-    #Blob().__init__(self, color, x_boundary, y_boundary)
-    super().__init__(color, x_boundary, y_boundary)
-    self.color = BLUE
-  
-  def move_fast(self):
-    self.x += random.randrange(-7,7)
-    self.y += random.randrange(-7,7)
 
+class BlueBlob(Blob):
+    def __init__(self, color, x_boundary, y_boundary):
+        Blob.__init__(self, color, x_boundary, y_boundary)
+        self.color = BLUE
+    def move_fast(self):
+        self.x += random.randrange(-5,5)
+        self.y += random.randrange(-5,5)
 
 
 def draw_environment(blob_list):
     game_display.fill(WHITE)
-
     for blob_dict in blob_list:
         for blob_id in blob_dict:
             blob = blob_dict[blob_id]
             pygame.draw.circle(game_display, blob.color, [blob.x, blob.y], blob.size)
             blob.move_fast()
             blob.check_bounds()
-
     pygame.display.update()
-    
 
 def main():
-  #
     blue_blobs = dict(enumerate([BlueBlob(BLUE,WIDTH,HEIGHT) for i in range(STARTING_BLUE_BLOBS)]))
     red_blobs = dict(enumerate([BlueBlob(RED,WIDTH,HEIGHT) for i in range(STARTING_RED_BLOBS)]))
     while True:
@@ -54,6 +50,5 @@ def main():
                 quit()
         draw_environment([blue_blobs,red_blobs])
         clock.tick(60)
-
-if __name__ == '__main__':
+if __name__ == ‘__main__‘:
     main()
