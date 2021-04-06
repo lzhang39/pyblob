0662269a3f12670350fbe42b4156ac07f78473f4
lzhang39 <>
2021-04-06 15:59:44 +0000

    includes more functionality for the user (modular)

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #red_blob = Blob(RED)
 
-
 import pygame
 import random
+# from blob import Blob
+
 
 STARTING_BLUE_BLOBS = 10
 STARTING_RED_BLOBS = 3
@@ -19,23 +20,27 @@ clock = pygame.time.Clock()
 
 class Blob:
 
-    def __init__(self, color):
-        self.x = random.randrange(0, WIDTH)
-        self.y = random.randrange(0, HEIGHT)
-        self.size = random.randrange(4,8)
+    def __init__(self, color, x_boundary, y_boundary, size_range=(4,8), movement_range=(-1,2)):
+        self.size = random.randrange(size_range[0],size_range[1])
         self.color = color
+        self.x_boundary = x_boundary
+        self.y_boundary = y_boundary
+        self.x = random.randrange(0, self.x_boundary)
+        self.y = random.randrange(0, self.y_boundary)
+        self.movement_range = movement_range
 
     def move(self):
-        self.move_x = random.randrange(-1,2)
-        self.move_y = random.randrange(-1,2)
+        self.move_x = random.randrange(self.movement_range[0],self.movement_range[1])
+        self.move_y = random.randrange(self.movement_range[0],self.movement_range[1])
         self.x += self.move_x
         self.y += self.move_y
 
+    def check_bounds(self):
         if self.x < 0: self.x = 0
-        elif self.x > WIDTH: self.x = WIDTH
+        elif self.x > self.x_boundary: self.x = self.x_boundary
         
         if self.y < 0: self.y = 0
-        elif self.y > HEIGHT: self.y = HEIGHT
+        elif self.y > self.y_boundary: self.y = self.y_boundary
 
 
 def draw_environment(blob_list):
@@ -51,8 +56,10 @@ def draw_environment(blob_list):
     
 
 def main():
-    blue_blobs = dict(enumerate([Blob(BLUE) for i in range(STARTING_BLUE_BLOBS)]))
-    red_blobs = dict(enumerate([Blob(RED) for i in range(STARTING_RED_BLOBS)]))
+  #
+    blue_blobs = dict(enumerate([Blob(BLUE,WIDTH,HEIGHT) for i in range(STARTING_BLUE_BLOBS)]))
+    red_blobs = dict(enumerate([Blob(RED,WIDTH,HEIGHT) for i in range(STARTING_RED_BLOBS)]))
+    while True:
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
